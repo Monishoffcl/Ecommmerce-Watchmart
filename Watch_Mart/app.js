29980cb2738158e254ff5df418feae9ae866dcde
Monish <monish.n18082004@gmail.com>
2025-11-13 00:41:21 +0530

    new commit

--- Watch_Mart/app.js
+++ Watch_Mart/app.js
@@ -6,52 +6,33 @@ const products=[{id:1,title:"RICHARD MILLE",price:5000,colours:[{code:"black",im
     id:4,title:"TITAN",price:1000,colours:[{code:"black",img:"./img/Pre-owned-rolex.jpg",},{code:"darkblue",img:"./img/car blue.jpg",}],
 },{id:5,title:"BOVET",price:6000,colours:[{code:"black",img:"./img/rolex-brand-luxe-digital.jpg",},{code:"darkblue",img:"./img/hip blue.jpg",}],}]
 
-let choosenproduct=products[0]
+let choosenproduct=products[0];
 const currentproductimg=document.querySelector(".productimg");
 const currentproducttitle=document.querySelector(".producttitle");
 const currentproductprice=document.querySelector(".productprice");
-const currentproductcolours=document.querySelectorAll(".colour");
-const currentproductsizes=document.querySelectorAll(".size");
 
 menuitems.forEach((item,index)=>{
     item.addEventListener("click",()=>{
         wrapper.style.transform=`translateX(${-100 * index}vw)`;
-        //CHANGE THE CHOOSEN PRODUCT
-        choosenproduct=products[index]
-        //change text of currentproduct
+        choosenproduct=products[index];
         currentproducttitle.textContent = choosenproduct.title;
         currentproductprice.textContent = "$"+choosenproduct.price;
         currentproductimg.src=choosenproduct.colours[0].img;
-        //asign new colour
-        currentproductcolours.forEach((colour,index)=>{
-            colour.style.backgroundcolour=choosenproduct.colours[index].code;
-        });
-        //currentproducttitle.textContent = choosenproduct.title;
-    });
-});
-currentproductcolours.forEach((colour,index)=>{
-    colour.addEventListener("click",()=>{
-        currentproductimg.src=choosenproduct.colours[index].img
-    });
-});
-currentproductsizes.forEach((size,index)=>{
-    size.addEventListener("click",()=>{
-        currentproductsizes.forEach(size=>{
-            size.style.backgroundcolor= "white";
-            size.style.color="black";
-
-        });
-    size.style.backgroundcolor= "black";
-    size.style.color="white";
-    
     });
 });
 const productbutton=document.querySelector(".productbutton");
 const payment=document.querySelector(".payment");
+const paymentOverlay=document.querySelector(".payment-overlay");
 const close=document.querySelector(".close");
 productbutton.addEventListener("click",()=>{
     payment.style.display="flex";
+    paymentOverlay.style.display="block";
 });
 close.addEventListener("click",()=>{
     payment.style.display="none";
+    paymentOverlay.style.display="none";
+});
+paymentOverlay.addEventListener("click",()=>{
+    payment.style.display="none";
+    paymentOverlay.style.display="none";
 });
